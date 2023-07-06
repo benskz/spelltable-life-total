7d09a0a3f8efe554b4ef79d02b28e2ef087a8ca6
benskz <bernardo@unlockingthedigital.com>
2023-07-06 23:55:45 +0100

    handle dom changes

--- spelltable-life-total.js
+++ spelltable-life-total.js
@@ -3,9 +3,6 @@ console.debug('Initializing Spelltable Life Total.');
 const DOM_POLL_INTERVAL = 3000;
 const COMMIT_TIMEOUT = 1500;
 
-let decrementButton = null;
-let incrementButton = null;
-
 const TOOLTIP_CLASS = 'spelltable-life-total-tooltip';
 const TOOLTIP_SHOW_CLASS = 'spelltable-life-total-tooltip-show';
 let deltaTooltip = null;
@@ -13,8 +10,54 @@ let deltaTooltip = null;
 let lifeDelta = 0;
 let lifeDeltaCommitTimeout = null;
 
+// finds container and removes overflow-hidden
+const prepareContainer = () => {
+  // container can be found 2 levels up from decrement button
+  const container = document.querySelector(
+    '[aria-label="Decrement Life"]'
+  ).parentElement.parentElement;
+
+  container.classList.remove('overflow-hidden');
+};
+
+const createTooltip = () => {
+  // add tooltip element to parent of buttons
+  const tooltip = document.createElement('div');
+  tooltip.classList.add(TOOLTIP_CLASS);
+  tooltip.innerText = '0';
+  document.querySelector(
+    '[aria-label="Decrement Life"]'
+  ).parentElement.appendChild(tooltip);
+  deltaTooltip = tooltip;
+};
+
+const commitLifeDelta = () => {
+  console.debug('Reset Life Delta:', lifeDelta);
+
+  // hide the tooltip
+  deltaTooltip.classList.remove(TOOLTIP_SHOW_CLASS);
+
+  setTimeout(() => {
+    // remove the tooltip
+    try {
+    deltaTooltip.remove();
+    } finally {
+      deltaTooltip = null;
+
+      // reset the life delta
+      lifeDelta = 0;
+    }
+  }, 300);
+};
+
 const updateLifeDelta = (delta) => {
   lifeDelta = lifeDelta + delta;
+
+  // add tooltip if it doesn't exist
+  if (!document.querySelector(`.${TOOLTIP_CLASS}`)) {
+    prepareContainer();
+    createTooltip();
+  }
 
   // update the tooltip, add + if positive
   deltaTooltip.innerText = lifeDelta > 0 ? `+${lifeDelta}` : lifeDelta;
@@ -26,13 +69,7 @@ const updateLifeDelta = (delta) => {
 
   // set a timeout to commit the life delta
   lifeDeltaCommitTimeout = setTimeout(() => {
-    console.debug('Reset Life Delta:', lifeDelta);
-
-    // hide the tooltip
-    deltaTooltip.classList.remove(TOOLTIP_SHOW_CLASS);
-
-    // reset the life delta
-    lifeDelta = 0;
+    commitLifeDelta();
 
     // clear the timeout
     lifeDeltaCommitTimeout = null;
@@ -41,49 +78,27 @@ const updateLifeDelta = (delta) => {
 
 const init = () => {
   // set event listeners
-  decrementButton.addEventListener('click', (e) => {
-    updateLifeDelta(-1);
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+
+    // catch clicks on the button and child svg and line elements
+    if (
+      target.matches('[aria-label="Decrement Life"]') ||
+      target.parentElement.matches('[aria-label="Decrement Life"]') ||
+      target.parentElement.parentElement.matches('[aria-label="Decrement Life"]')
+      ) {
+      updateLifeDelta(-1);
+    } else if (
+      target.matches('[aria-label="Increment Life"]') ||
+      target.parentElement.matches('[aria-label="Increment Life"]') ||
+      target.parentElement.parentElement.matches('[aria-label="Increment Life"]')
+    ) {
+      updateLifeDelta(1);
+    }
   });
-
-  incrementButton.addEventListener('click', (e) => {
-    updateLifeDelta(1);
-  });
-
-  // add tooltip element to parent of buttons
-  const tooltip = document.createElement('div');
-  tooltip.classList.add(TOOLTIP_CLASS);
-  tooltip.innerText = '0';
-  decrementButton.parentElement.appendChild(tooltip);
-  deltaTooltip = tooltip;
-
-  // remove overflow-hidden from container
-  // container can be found 2 levels up from decrement button
-  const container = decrementButton.parentElement.parentElement;
-  container.classList.remove('overflow-hidden');
 
   console.debug('Successfully Initialized Spelltable Life Total.');
 };
 
-// Check for decrement and increment buttons
-const checkForRequiredElements = () => {
-  // find the decrement button by aria-label="Decrement Life"
-  const decrementButtonCheck = document.querySelector(
-    '[aria-label="Decrement Life"]'
-  );
-  // find the increment button by aria-label="Increment Life"
-  const incrementButtonCheck = document.querySelector(
-    '[aria-label="Increment Life"]'
-  );
-
-  if (decrementButtonCheck && incrementButtonCheck) {
-    decrementButton = decrementButtonCheck;
-    incrementButton = incrementButtonCheck;
-    init();
-  } else {
-    console.debug('Could not find required DOM elements for initialization.');
-    setTimeout(checkForRequiredElements, DOM_POLL_INTERVAL);
-  }
-};
-
-// Start polling for required elements
-checkForRequiredElements();
+// Initialize the extension
+init();
